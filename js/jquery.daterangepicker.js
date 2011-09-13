53dbd1faeb224767573fa9321b5eebe84517d56d
Hedrobino <rodolphe.belouin@gmail.com>
2011-09-13 16:45:18 +0200

    Preset Ranges are working !

--- js/jquery.daterangepicker.js
+++ js/jquery.daterangepicker.js
@@ -5,14 +5,14 @@ jQuery.fn.daterangepicker = function(settings) {
     var options = jQuery.extend({
         presetRanges: [
             {text: 'Yesterday', ranges:[{s: 'today-1days', e: 'today-1days'}]},
-            {text: 'Last 7 days', ranges:[{s: 'today-7days', e: 'today'}]}
+            {text: 'Last 7 days', ranges:[{s: 'today-7days', e: 'today'}]},
+            {text: 'Compare the two last weeks', ranges:[{s:'today-7days', e:'today'}, {s:'today-15days', e: 'today-8days'}]}
         ],
         presets: {
-            singleDate: 'Specific date',
-            singleRange: 'Date range',
-            twoRanges: 'Compare ranges'
+            singleDate: {title: 'Specific date', n: 0},
+            singleRange: {title: 'Date range', n: 1, labels: ['Start date', 'End date']},
+            twoRanges: {title: 'Compare ranges', n: 2, labels: [['DS1', 'DE1'], ['DS2', 'DE2']]}
         },
-        titles: [['DS1', 'DE1'], ['DS2', 'DE2']],
         doneButtonText: 'Done',
         rangeSplitter: '-',
         rangeSeparator: '|',
@@ -52,26 +52,52 @@ jQuery.fn.daterangepicker = function(settings) {
     var rp = jQuery('<div class="ui-daterangepicker ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" style="display:none"></div>');
     var rpPresets = (function() {
         var ul = jQuery('<ul></ul>').appendTo(rp);
+        // PresetRanges
         jQuery.each(options.presetRanges, function() {
             var li = jQuery('<li class="ui-daterangepicker-' + this.text.replace(/ /g, '') + ' ui-corner-all"><a href="#" rel="' + this.rel + '" title="' + this.text + '">' + this.text + '</a></li>').data('ranges', this.ranges);
             ul.append(li);
         });
-
-        ul.find('li')
-            .hover(
-                function() {
-                    jQuery(this).addClass('ui-state-hover');
-                },
-                function() {
-                    jQuery(this).removeClass('ui-state-hover');
-                })
-            .click(function() {
-                rp.find('.ui-state-active').removeClass('ui-state-active');
-                jQuery(this).addClass('ui-state-active');
-                return false;
-            });
+        // Presets
+        jQuery.each(options.presets, function() {
+            var li = jQuery('<li class="ui-corner-all"><a href="#" rel="' + this.rel + '" title="' + this.title + '">' + this.title + '</a></li>').data('presetSettings', this);
+            ul.append(li);
+        });
         return ul;
     })();
+	var rpPickersBoxes = jQuery('<div class="ui-ranges ui-widget-header ui-corner-all ui-helper-clearfix" style="display:none"></div>').appendTo(rp);
+    var doneBtn = jQuery('<button class="btonDone ui-state-default ui-corner-all">' + options.doneButtonText + '</button>');
+
+     /////////////////////////////////////////
+    // LET'S MANAGE DATERANGEPICKER EVENTS //
+   /////////////////////////////////////////
+    rpPresets.find('li')
+        .hover(
+            function() {
+                jQuery(this).addClass('ui-state-hover');
+            },
+            function() {
+                jQuery(this).removeClass('ui-state-hover');
+            })
+        .click(function() {
+            rp.find('.ui-state-active').removeClass('ui-state-active');
+            jQuery(this).addClass('ui-state-active').clickActions(rp, rpPickersBoxes, doneBtn);
+            return false;
+        });
+
+    jQuery.fn.clickActions = function(rp, rpPickersBoxes, doneBtn) {
+        if($(this).data('ranges')) {
+            var r = $(this).data('ranges'), t = [];
+            for(var i in r) {
+                var s = jQuery.datepicker.formatDate(options.dateFormat, Date.parse(r[i].s));
+                var e = jQuery.datepicker.formatDate(options.dateFormat, Date.parse(r[i].e));
+                t.push(s + options.rangeSplitter + e);
+            }
+            input.val(t.join(options.rangeSeparator));
+        }
+        else {
+            console.debug($(this).data('presetSettings'));
+        }
+    };
 
     // Show, hide, or toggle rangepicker
     function show() {
